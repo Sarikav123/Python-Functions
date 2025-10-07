463bfc066aa1ddc1d4bdc593403c1bd64dae4a01
Sarikav123 <72382945+Sarikav123@users.noreply.github.com>
2025-10-07 11:21:19 +0530

    Create len.py

---
+++ len.py
@@ -0,0 +1,10 @@
+# What does the len() function do in Python?
+# the len() function returns the number of items in an object:
+# For a string, it counts characters.
+# For a list, tuple, or dictionary, it counts elements.
+#Write a code example using len() to find the length of a list.
+
+Fruits = ['Apple','Banana','Cherry']
+frlen = len(Fruits)
+print(f' Number of fruits in the list is {frlen}')
+
