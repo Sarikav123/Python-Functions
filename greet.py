a223c5a7856c87985b58480447601fcdd97bdcc6
Sarikav123 <72382945+Sarikav123@users.noreply.github.com>
2025-10-07 11:21:54 +0530

    Create greet.py

---
+++ greet.py
@@ -0,0 +1,5 @@
+#Write a Python function greet(name) that takes a person's name as input and prints Hello, [name]!.
+name = input('Enter your name: ')
+def greet(name):
+    print(f'Hello {name}')
+greet(name)
